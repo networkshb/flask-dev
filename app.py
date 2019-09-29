8bdbdaf0d1478d568deeba2cea888a9d68c87bf0
networkshb <networkshb@163.com>
2019-09-29 10:17:29 +0800

    add email support

--- app.py
+++ app.py
@@ -8,19 +8,31 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_mail import Message
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
+app.config['MAIL_SERVER'] = 'smtp.163.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWD')
+app.config['MAIL_SUBJECT_PREFIX'] = '[Flask]'
+app.config['MAIL_SENDER'] = os.environ.get('MAIL_SENDER')
+app.config['MAIL_ADMIN'] = os.environ.get('MAIL_ADMIN')
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+mail = Mail(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
@@ -44,6 +56,20 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + subject, \
+            sender=app.config['MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name = None
@@ -55,6 +81,9 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['MAIL_ADMIN']:
+                send_email(app.config['MAIL_ADMIN'], 'New User',
+               'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
