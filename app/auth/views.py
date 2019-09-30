89c3bea4a5ec8ad54362949518d37c6ee101a389
networkshb <networkshb@163.com>
2019-09-30 14:03:29 +0800

    add Login & Logout with flask-login

---
+++ app/auth/views.py
@@ -0,0 +1,31 @@
+from flask import render_template, redirect, request, url_for, flash
+from . import auth
+from .forms import LoginForm
+from flask_login import login_required, logout_user, login_user
+from ..models import User
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data.lower()).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Invalid email or password.')
+    return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out')
+    return redirect(url_for('main.index'))
+
+@auth.route('/sec')
+@login_required
+def secarea():
+    return '<h2>sec area</h2>'
